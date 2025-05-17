587ea2b40052b61b6ba4a29d3c222a5d0c4c7604
Dv-TiagoCarvalho <tiagocarvalho0606@gmail.com>
2025-05-17 19:41:33 -0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+         Fatura c1 = new Fatura(5,"memoria", 10, 300);
+
+        System.out.println("Numero do item: " + c1.numeroFaturado);
+        System.out.println("Descrição do item: " + c1.descriçao);
+        System.out.println("Quantidade Compra: " + c1.quantidadeCompra);
+        System.out.println("Preço: " + c1.preço);
+        System.out.println("Valor Fatura: " + c1.calcularValorFatura());
+        
+
+        
+    }   
+}
